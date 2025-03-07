a70bee8a08011deb02b9c021eae3e10e79efe9e5
Michael Tews <michael@tews.dev>
2025-03-07 09:43:50 +0100

    feat: install extensions
    Signed-off-by: Michael Tews <michael@tews.dev>

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -92,22 +92,29 @@ gsettings set "org.gnome.desktop.wm.keybindings" "close" "['<Shift><Super>q']"
 gsettings set "org.gnome.desktop.wm.keybindings" "toggle-maximized" "['<Super>f']"
 
 echo "Configuring workspace and application keybindings..."
-gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-hotkey-${0}" "[]"
 for i in {1..6}; do 
     gsettings set "org.gnome.shell.keybindings" "switch-to-application-${i}" "[]"
     gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-${i}" "['<Super>${i}']"
     gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-${i}" "['<Super><Shift>${i}']"
-    gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-ctrl-hotkey-${i}" "[]"
-    gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-hotkey-${i}" "[]"
 done
 
+array=( https://extensions.gnome.org/extension/3193/blur-my-shell/
+https://extensions.gnome.org/extension/517/caffeine/
+https://extensions.gnome.org/extension/615/appindicator-support/
+https://extensions.gnome.org/extension/16/auto-move-windows/ )
 
-# Prompt to install intlde keyboard-layout
-read -p "Install intlde keyboard-layout? (y/n): " answer
-if [[ "$answer" =~ ^[Yy]$ ]]; then
-    echo "Installing intlde keyboard layout..."
-    ./intlde-install.py
-fi
+for i in "${array[@]}"
+do
+    EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
+    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+    curl -L "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG" -o ${EXTENSION_ID}.zip
+    gnome-extensions install --force ${EXTENSION_ID}.zip
+    if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
+        busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
+    fi
+    gnome-extensions enable ${EXTENSION_ID}
+    rm ${EXTENSION_ID}.zip
+done
 
 # Install Adwaita GTK3 theme and configure it
 echo "Installing Adwaita GTK3 theme..."
